6f0a5df0374170fec84ee361309c5e9bebde5e74
Felice La Rizza <larizza.felice@gmail.com>
2022-12-21 21:35:17 +0100

    Clarify code (line per line) with comments

--- GildedRose_first/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose_first/src/main/java/com/gildedrose/GildedRose.java
@@ -18,10 +18,11 @@ class GildedRose {
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 //The Quality of an item is never negative
                 if (items[i].quality > 0) {
-                    //
+                    //Sulfuras can not lose quality (so never -1 in quality)
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
                     }
+
                 }
                 //talking about items which value rise each day (=Backstage Pass)
             } else {
@@ -31,26 +32,29 @@ class GildedRose {
                     items[i].quality = items[i].quality + 1;
                     //"Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        // if sellIn is 10 days or less and if quality is less than 50 , then increase quality by 1
+                        // at this point quality will be increased by 2
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 //second day of two : Quality increases by 2 when there are 10 days or less
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
-
+                        // if sellIn is 5 days or less and if quality is less than 50 , then increase quality by 1
+                        // at this point quality will be increased by 3
                         if (items[i].sellIn < 6) {
                             if (items[i].quality < 50) {
-                                // by 3 when there are 5 days or less but
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
                     }
                 }
             }
-
+            // SellIn value has to be decreased by 1 to all product except for Sulfuras
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
+            //same code as first block under for loop - does the same!
             if (items[i].sellIn < 0) {
                 // if name = Aged Brie, the go to ELSE
                 if (!items[i].name.equals("Aged Brie")) {

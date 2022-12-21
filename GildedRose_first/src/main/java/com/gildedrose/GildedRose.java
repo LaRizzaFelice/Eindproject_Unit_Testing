548a850a49a04450946663ad694f684babf3bab3
Felice La Rizza <larizza.felice@gmail.com>
2022-12-21 21:35:26 +0100

    Changes by WimDeCuyperDigitOffi

--- GildedRose_first/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose_first/src/main/java/com/gildedrose/GildedRose.java
@@ -14,29 +14,31 @@ class GildedRose {
     public void updateQuality() {
         // we iterate through our items indexes with a for loop going from 0 to the length of our itemlist
         for (int i = 0; i < items.length; i++) {
+            // if itemname is Aged Brie and/or Backstage passes, then go to ELSE
+            // if itemname is NOT aged brie AND NOT backstage passes, then go to next IF
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 //The Quality of an item is never negative
                 if (items[i].quality > 0) {
                     //Sulfuras can not lose quality (so never -1 in quality)
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                        //decrease quality of all items BESIDE Aged Brie, Backstage Passes and Sulfuras by 1
                         items[i].quality = items[i].quality - 1;
                     }
 
                 }
-                //talking about items which value rise each day (=Backstage Pass)
+                //talking about items which value increase each day (Backstage Passes and Aged Brie)
             } else {
                 //The Quality of an item is never more than 50
+                //increases the quality of Aged Brie and Backstage Passes by 1
                 if (items[i].quality < 50) {
-                    //first day of two : Quality increases by 2 when there are 10 days or less
                     items[i].quality = items[i].quality + 1;
-                    //"Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+                    //"Backstage passes" increases in Quality as its SellIn value approaches;
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         // if sellIn is 10 days or less and if quality is less than 50 , then increase quality by 1
                         // at this point quality will be increased by 2
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
-                                //second day of two : Quality increases by 2 when there are 10 days or less
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
@@ -63,7 +65,7 @@ class GildedRose {
                         if (items[i].quality > 0) {
                             //Sulfuras can not lose quality (so never -1 in quality)
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                //all items, beside Aged Brie, Backstage PAsses and Sulfuras lower 1 in quality
+                                //all items, beside Aged Brie, Backstage Passes and Sulfuras lower 1 in quality
                                 items[i].quality = items[i].quality - 1;
                             }
                         }

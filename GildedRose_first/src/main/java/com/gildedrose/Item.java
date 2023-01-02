df6c699dc00e1d9905ca71da3c4c4f90d45c46e1
Felice La Rizza <larizza.felice@gmail.com>
2023-01-02 21:09:47 +0100

    Itemklasse aangepast + main klasse (TexttestFixture) aangepast met nieuw veld en constructor

--- GildedRose_first/src/main/java/com/gildedrose/Item.java
+++ GildedRose_first/src/main/java/com/gildedrose/Item.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+
 public class Item {
 
     public String name;
@@ -8,14 +9,16 @@ public class Item {
 
     public int quality;
 
-    public Item(String name, int sellIn, int quality) {
+    public boolean isDegradable;
+
+    public Item(String name, int sellIn, int quality, boolean isDegradable) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
+        this.isDegradable = isDegradable;
     }
 
-   @Override
-   public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
-    }
+    @Override
+    public String toString() {
+        return this.name + ", " + this.sellIn + ", " + this.quality; }
 }
